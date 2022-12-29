21ef11cb1d646ee1104bff5909badb62712f3d9d
Igor Jorge <igorjplino@gmail.com>
2022-12-29 08:04:23 +0000

    ArgumentNullException if data of the new Node is null

--- src/LinkedList/Node.cs
+++ src/LinkedList/Node.cs
@@ -2,11 +2,13 @@
 {
     internal class Node<T>
     {
-        public T? Data { get; }
+        public T Data { get; }
         public Node<T>? Next { get; set; }
 
         public Node(T data)
         {
+            ArgumentNullException.ThrowIfNull(data);
+
             Data = data;
             Next = default;
         }
