3af79c89c115231e38cfaf3817de020d809bbdd4
Igor Jorge <igorjplino@gmail.com>
2023-01-04 07:43:16 +0000

    Fineshed implementation and unit tests of LinkedList

---
+++ src/Test/LinkedListTests/GetValueByIndexTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Test.LinkedListTests
+{
+    public class GetValueByIndexTests
+    {
+        private readonly LinkedList.LinkedList<int> _linkedList;
+
+        public GetValueByIndexTests()
+        {
+            _linkedList = new LinkedList.LinkedList<int>();
+        }
+
+        [Fact]
+        public void GivenList_WhenGetValue_ShouldReturnExpectedValue()
+        {
+            // Arrange
+
+            foreach (var item in Enumerable.Range(0, 10))
+            {
+                _linkedList.Append(item);
+            }
+
+            var expectedValue = 3;
+            var index = 3;
+
+            // Act
+            var result = _linkedList.GetValueByIndex(index);
+
+            // Assert
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Fact]
+        public void GivenList_WhenInformedIndexOutOfRange_ShouldThrowIndexOutOfRangeException()
+        {
+            // Arrange
+
+            foreach (var item in Enumerable.Range(0, 10))
+            {
+                _linkedList.Append(item);
+            }
+
+            var index = 99;
+
+            // Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => _linkedList.GetValueByIndex(index));
+        }
+    }
+}
---
+++ src/Test/LinkedListTests/RemoveTests.cs
@@ -0,0 +1,61 @@
+﻿namespace Test.LinkedListTests
+{
+    public class RemoveTests
+    {
+        private readonly LinkedList.LinkedList<int> _linkedList;
+
+        public RemoveTests()
+        {
+            _linkedList = new LinkedList.LinkedList<int>();
+        }
+
+        [Fact]
+        public void GivenList_WhenRemoveValue_ShouldReturnInvalidIndexWhendInformedNonexistentValue()
+        {
+            // Arrange
+
+            foreach (var item in Enumerable.Range(0, 10))
+            {
+                _linkedList.Append(item);
+            }
+
+            var valueToRemove = 9;
+
+            // Act
+
+            _linkedList.Remove(valueToRemove);
+
+            // Assert
+
+            var index = _linkedList.GetIndex(valueToRemove);
+            Assert.True(index < 0);
+        }
+
+        [Fact]
+        public void GivenEmptyList_WhenTryRemoveValue_ShouldThrowException()
+        {
+            // Arrange
+            var valueToRemove = 9;
+
+            // Act & Assert
+
+            Assert.Throws<Exception>(() => _linkedList.Remove(valueToRemove));
+        }
+
+        [Fact]
+        public void GivenList_WhenValueToRemoveDoesNotExist_ShouldThrowException()
+        {
+            // Arrange
+
+            foreach (var item in Enumerable.Range(0, 10))
+            {
+                _linkedList.Append(item);
+            }
+
+            var valueToRemove = 99;
+
+            // Assert
+            Assert.Throws<Exception>(() => _linkedList.Remove(valueToRemove));
+        }
+    }
+}
