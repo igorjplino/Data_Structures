0a39fc9dba5630b02d53703afeb05de38b134f70
Igor Jorge <igorjplino@gmail.com>
2023-02-01 07:42:48 +0000

    Added unit tests for BinarySearchTree

---
+++ src/Test/BinaryTree/BinarySearchTreeTests/InsertTests.cs
@@ -0,0 +1,27 @@
+﻿using BinaryTree;
+
+namespace Test.BinaryTree.BinarySearchTreeTests;
+
+public class InsertTests
+{
+    [Fact]
+    public void InsertedValues_ShouldContainInOrderTraversal()
+    {
+        // Arrange
+        var random = new Random(77);
+        var values = Enumerable.Range(0, 100).OrderBy(_ => random.Next()).ToList();
+
+        var bst = new BinarySearchTree();
+
+        // Act
+        foreach (var value in values)
+            bst.Insert(value);
+
+        // Assert
+
+        var result = bst.InOrderTraversal();
+
+        result.Should()
+            .Contain(values);
+    }
+}
---
+++ src/Test/BinaryTree/BinarySearchTreeTests/SearchTests.cs
@@ -0,0 +1,46 @@
+﻿using BinaryTree;
+
+namespace Test.BinaryTree.BinarySearchTreeTests;
+
+public class SearchTests
+{
+    [Fact]
+    public void SearchValue_ShouldContainInTree()
+    {
+        // Arrange
+        var random = new Random(77);
+        var values = Enumerable.Range(0, 100).OrderBy(_ => random.Next()).ToList();
+
+        var bst = new BinarySearchTree();
+
+        // Act
+        foreach (var value in values)
+            bst.Insert(value);
+
+        // Assert
+        var expected = values.FirstOrDefault(o => o.Equals(random.Next()));
+        var result = bst.Search(expected);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void SearchNonExistingValue_ShouldReturnNull()
+    {
+        // Arrange
+        var random = new Random(77);
+        var values = Enumerable.Range(0, 100).OrderBy(_ => random.Next()).ToList();
+
+        var bst = new BinarySearchTree();
+
+        // Act
+        foreach (var value in values)
+            bst.Insert(value);
+
+        // Assert
+        var nonExistingValue = values.Max() + 1;
+        var result = bst.Search(nonExistingValue);
+
+        Assert.Null(result);
+    }
+}
